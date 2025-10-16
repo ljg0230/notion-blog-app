6f5f5aba3fb8e9cfdf0ecbe38e9c34684746698b
leejg <ljg0230@gmail.com>
2025-10-16 23:07:56 +0900

    chore: mdx 패키지 추가

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from 'next';
+import createMDX from '@next/mdx';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -15,6 +16,12 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // 필요 마크다운 추가
+  // extension: /\.mdx?$/,
+});
+
+export default withMDX(nextConfig);
