98cdc7ba172dbb590b72a2c4d045f734548d4e77
leejg <ljg0230@gmail.com>
2025-10-20 23:18:03 +0900

    chore: remark and rehype plugin 설치

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 import type { NextConfig } from 'next';
 import createMDX from '@next/mdx';
+// import remarkGfm from 'remark-gfm';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -22,6 +23,13 @@ const nextConfig: NextConfig = {
 const withMDX = createMDX({
   // 필요 마크다운 추가
   // extension: /\.mdx?$/,
+  options: {
+    // webpack 버전 사용 시
+    // remarkPlugins: [remarkGfm],
+    // ts-expect-error remark-gfm 타입 충돌 문제 해결
+    // turbo 버전 사용 시
+    // remarkPlugins: [['remarkGfm']],
+  },
 });
 
 export default withMDX(nextConfig);
